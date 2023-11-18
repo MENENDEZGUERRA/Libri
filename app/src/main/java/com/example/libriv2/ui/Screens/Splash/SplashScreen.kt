541dda4df7706c8ef00426801c1fab730b1536d8
Edwin Ortega <andyortegakou@yahoo.com.mx>
2023-11-18 17:50:20 -0600

    Splashs edited

--- app/src/main/java/com/example/libriv2/ui/Screens/Splash/SplashScreen.kt
+++ app/src/main/java/com/example/libriv2/ui/Screens/Splash/SplashScreen.kt
@@ -3,6 +3,7 @@ package com.example.libriv2.ui.Screens.Splash
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
@@ -11,6 +12,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,18 +32,31 @@ fun SplashScreen(navController: NavHostController){
 }
 
 @Composable
-fun Splash(){
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+fun Splash() {
+    BoxWithConstraints(
+        modifier = Modifier.fillMaxSize()
     ) {
+        val backgroundImage = painterResource(id = R.drawable.logofondo)
         Image(
-            painter = painterResource(id = R.drawable.librilogo),
-            contentDescription = "Logo Libri",
-            Modifier.size(250.dp, 250.dp))
+            painter = backgroundImage,
+            contentDescription = "Fondo",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.matchParentSize()
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.librilogo),
+                contentDescription = "Logo Libri",
+                Modifier.size(250.dp, 250.dp)
+            )
+        }
     }
 }
 
